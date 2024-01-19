ae1d7bd8d05e07347ee8627d4fdd19e1c2ee2132
Pablo Chesini <pchesini1@gmail.com>
2024-01-19 08:45:28 -0300

    .

---
+++ src/test/java/com/ApiResto/Resto/RestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiResto.Resto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
